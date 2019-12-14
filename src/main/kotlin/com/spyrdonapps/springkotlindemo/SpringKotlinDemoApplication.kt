e6938b25aeedab5ee0c240c091b31956d0cdbbc3
patrycjusz.blaszczuk <patrycjusz.blaszczuk@gmail.com>
2019-12-14 01:34:13 +0100

    Kotlin init

---
+++ src/main/kotlin/com/spyrdonapps/springkotlindemo/SpringKotlinDemoApplication.kt
@@ -0,0 +1,12 @@
+package com.spyrdonapps.springkotlindemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinDemoApplication>(*args)
+	println("XD")
+}
