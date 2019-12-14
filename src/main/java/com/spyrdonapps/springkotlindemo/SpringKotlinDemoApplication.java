386e46aa1d0348b3af810af36ab2112a74e759c6
solidogen <solidogen@gmail.com>
2019-12-14 01:21:36 +0100

    Initial

---
+++ src/main/java/com/spyrdonapps/springkotlindemo/SpringKotlinDemoApplication.java
@@ -0,0 +1,13 @@
+package com.spyrdonapps.springkotlindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKotlinDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKotlinDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spyrdonapps/springkotlindemo/SpringKotlinDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spyrdonapps.springkotlindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKotlinDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
