c4a6d54d9b916de7a5cba3904374acac9df28ae7
patrycjusz.blaszczuk <patrycjusz.blaszczuk@gmail.com>
2019-12-14 15:11:49 +0100

    CSS enabled

---
+++ src/main/kotlin/com/spyrdonapps/springkotlinsample/UiConfiguration.kt
@@ -0,0 +1,18 @@
+package com.spyrdonapps.springkotlinsample
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class UiConfiguration : WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/**")
+            .addResourceLocations(
+                // THESE TWO DON'T WORK WITHOUT EACH OTHER, DON'T TOUCH IT
+                "classpath:/css",
+                "classpath:/static/css"
+            )
+            .setCachePeriod(0)
+    }
+}
